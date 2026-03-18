a8ca30c16458d4eec618909d90765702b4d0b6c4
HEMANTHH05 <hemanthkoppaka1628@gmail.com>
2026-03-17 21:44:13 -0500

    Add interactive chat with memory for Rohit Sharma info

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+
 def main():
     info = """Rohit Gurunath Sharma (born 30 April 1987) is an Indian international cricketer and the former captain of the India national cricket team in all formats of the game.[3] He is a right-handed top-order batter. He represents Mumbai in domestic cricket and Mumbai Indians in the Indian Premier League. Sharma was a member of the teams that won the 2007 T20 World Cup, the 2013 ICC Champions Trophy and was the winning captain of the 2024 T20 World Cup and the 2025 ICC Champions Trophy.
 
@@ -13,20 +14,34 @@ def main():
 
     He has received two national honours, the Arjuna Award in 2015 and the prestigious Khel Ratna Award in 2020 by the Government of India. Under his captaincy, India won the 2018 Asia Cup and the 2023 Asia Cup, the seventh and eighth time the country won the title, both in ODI format as well as the 2018 Nidahas Trophy, their second overall and first in T20I format. """
 
-    summary_template = """ Given the information {infoo} about the person tell about the person in two lines"""
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant. Answer ONLY using the information provided below. Do not use any outside knowledge. If the answer is not in the information, say 'I don't know'. Information: {info}"),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{question}")
+    ])
 
-    summary_prompt_tempalate = PromptTemplate(
-        input_variables=["infoo"],
-        template=summary_template
-    )
-    llm = ChatOpenAI(
-        model="gpt-3.5-turbo",
-        temperature=0
-    )
-    chain = summary_prompt_tempalate | llm
-    response = chain.invoke({"infoo": info})
-    print(response.content)
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    chain = prompt | llm
 
+    chat_history = []
+
+    print("Chat started! Ask anything about Rohit Sharma. Type 'exit' to quit.\n")
+
+    while True:
+        question = input("You: ")
+        if question.lower() == "exit":
+            break
+
+        response = chain.invoke({
+            "info": info,
+            "chat_history": chat_history,
+            "question": question
+        })
+
+        print(f"AI: {response.content}\n")
+
+        chat_history.append(HumanMessage(content=question))
+        chat_history.append(AIMessage(content=response.content))
 
 
 if __name__ == "__main__":
