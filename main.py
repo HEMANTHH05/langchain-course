3f1ba42edba5ded9ce4ec99bc84ba124e245983e
HEMANTHH05 <hemanthkoppaka1628@gmail.com>
2026-03-17 19:59:44 -0500

    added langsmith tracing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
 def main():
-    information = """Rohit Gurunath Sharma (born 30 April 1987) is an Indian international cricketer and the former captain of the India national cricket team in all formats of the game.[3] He is a right-handed top-order batter. He represents Mumbai in domestic cricket and Mumbai Indians in the Indian Premier League. Sharma was a member of the teams that won the 2007 T20 World Cup, the 2013 ICC Champions Trophy and was the winning captain of the 2024 T20 World Cup and the 2025 ICC Champions Trophy.
+    info = """Rohit Gurunath Sharma (born 30 April 1987) is an Indian international cricketer and the former captain of the India national cricket team in all formats of the game.[3] He is a right-handed top-order batter. He represents Mumbai in domestic cricket and Mumbai Indians in the Indian Premier League. Sharma was a member of the teams that won the 2007 T20 World Cup, the 2013 ICC Champions Trophy and was the winning captain of the 2024 T20 World Cup and the 2025 ICC Champions Trophy.
 
     Sharma holds several batting records which include most runs in T20 Internationals, most sixes in international cricket,[a] most double centuries in ODI cricket (3), most centuries at Cricket World Cups (7) and joint most hundreds in Twenty20 Internationals (5).[5] He also holds the world record for the highest individual score (264) in a One Day International (ODI) and also holds the record for scoring most hundreds (five) in a single Cricket World Cup, for which he won the ICC Men's ODI Cricketer of the Year award in 2019.[6] He is the first and only captain to lead a team in all[b] ICC tournament finals.[7]
 
@@ -13,10 +13,10 @@ def main():
 
     He has received two national honours, the Arjuna Award in 2015 and the prestigious Khel Ratna Award in 2020 by the Government of India. Under his captaincy, India won the 2018 Asia Cup and the 2023 Asia Cup, the seventh and eighth time the country won the title, both in ODI format as well as the 2018 Nidahas Trophy, their second overall and first in T20I format. """
 
-    summary_template = """ Given the information {information} about the person tell about the person in two lines"""
+    summary_template = """ Given the information {infoo} about the person tell about the person in two lines"""
 
     summary_prompt_tempalate = PromptTemplate(
-        input_variables=["information"],
+        input_variables=["infoo"],
         template=summary_template
     )
     llm = ChatOpenAI(
@@ -24,7 +24,7 @@ def main():
         temperature=0
     )
     chain = summary_prompt_tempalate | llm
-    response = chain.invoke({"information": information})
+    response = chain.invoke({"infoo": info})
     print(response.content)
 
 
